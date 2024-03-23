40605748a12a0432761c9aab3bb2f8ba913a2bfd
maksymPtashnyk <super.zet65@gmail.com>
2024-03-24 00:01:25 +0200

    create app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ReviewForm from './ReviewForm';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/maugli" element={<ReviewForm park="Мауглі" />} />
+        <Route path="/manki" element={<ReviewForm park="Манкі" />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/ReviewForm.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import { Input, Button, Box } from '@mui/material';
+
+const ReviewForm = ({park}) => {
+  const [review, setReview] = useState('');
+  const [userName, setUserName] = useState('');
+
+  const handleChangeFeedback = (event) => {
+    setReview(event.target.value);
+  };
+  const handleChangeName = (event) => {
+    setUserName(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const data = {
+      userName: userName,
+      review: review,
+      park,
+    };
+
+    try {
+      const response = await fetch('http://localhost:3001/feedback', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+
+      if (response.ok) {
+        console.log('Відгук надіслано успішно');
+      } else {
+        console.error('Помилка надсилання відгуку');
+      }
+    } catch (error) {
+      console.error('Помилка надсилання відгуку:', error);
+    }
+
+    setReview('');
+    setUserName('');
+  };
+
+  return (
+    <Box sx={{ maxWidth: 400, margin: 'auto', padding: 2, backgroundColor: 'azure' }}>
+      <h2>Залиште свій відгук про парк {park}</h2>
+      <form onSubmit={handleSubmit}>
+        <Input
+          label="Ваше ім'я"
+          multiline
+          type='text'
+          disableUnderline={true}
+          value={userName}
+          onChange={handleChangeName}
+          fullWidth
+          placeholder="Ваше ім'я"
+          required
+          sx={{
+            backgroundColor: 'white',
+            borderRadius: '4px',
+            padding: '5px',
+            marginBottom: '20px',
+          }}
+        />
+        <Input
+          label="Ваш відгук"
+          multiline
+          disableUnderline={true}
+          value={review}
+          onChange={handleChangeFeedback}
+          fullWidth
+          placeholder='Ваш відгук'
+          required
+          sx={{
+            backgroundColor: 'white',
+            borderRadius: '4px',
+            padding: '5px',
+            marginBottom: '20px',
+          }}
+        />
+        <Button type="submit" variant="contained" color="primary">
+          Надіслати відгук
+        </Button>
+      </form>
+    </Box>
+  );
+};
+
+export default ReviewForm;
