ac096536292739fe4d04b9585305e46f68421f28
maksymPtashnyk <super.zet65@gmail.com>
2024-03-25 15:50:20 +0200

    added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ReviewForm from './ReviewForm';
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
         <Route path="/maugli" element={<ReviewForm park="Мауглі" />} />
         <Route path="/manki" element={<ReviewForm park="Манкі" />} />
+        <Route path="*" element={<h1>Error</h1>} />
       </Routes>
     </Router>    
   );
